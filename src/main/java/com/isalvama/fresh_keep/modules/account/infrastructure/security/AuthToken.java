28ed53476ce16c6c5fd08c65ccdea5ba854c9f04
isalvama <inesalvans@gmail.com>
2026-07-15 10:07:36 +0200

    refactor: create InvalidTokenException and use it in AuthToken validator

--- src/main/java/com/isalvama/fresh_keep/modules/account/infrastructure/security/AuthToken.java
+++ src/main/java/com/isalvama/fresh_keep/modules/account/infrastructure/security/AuthToken.java
@@ -1,11 +1,13 @@
 package com.isalvama.fresh_keep.modules.account.infrastructure.security;
 
+import com.isalvama.fresh_keep.modules.account.domain.exception.InvalidTokenException;
+
 public record AuthToken(
         String token
 ) {
     public AuthToken {
         if (token == null || token.isBlank()){
-            throw new IllegalArgumentException("Token cannot be null or blank"); //TODO review
+            throw new InvalidTokenException("The Token cannot be null or blank");
         }
     }
 
