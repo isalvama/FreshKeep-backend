40a1d68ad28cd659f2da8b3ef6fe5bbf489c39ee
isalvama <inesalvans@gmail.com>
2026-07-20 18:48:09 +0200

    feat: create JwtAuthFilter class with implementation of OncePerRequestFilter's method doFilterInternal()

---
+++ src/main/java/com/isalvama/fresh_keep/modules/account/infrastructure/security/token/JwtAuthFilter.java
@@ -0,0 +1,62 @@
+package com.isalvama.fresh_keep.modules.account.infrastructure.security.token;
+
+import com.isalvama.fresh_keep.modules.account.domain.model.Account;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+@RequiredArgsConstructor
+public class JwtAuthFilter extends OncePerRequestFilter {
+
+    private final JwtTokenGeneratorAdapter jwtTokenGeneratorAdapter;
+
+    @Override
+    protected void doFilterInternal(
+            @NonNull HttpServletRequest request,
+            @NonNull HttpServletResponse response,
+            @NonNull FilterChain filterChain
+    ) throws ServletException, IOException {
+
+        String authHeader = request.getHeader("Authorization");
+
+        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        String jwtToken = authHeader.substring(7);
+
+        try {
+            if (jwtTokenGeneratorAdapter.isTokenValid(jwtToken)) {
+                CustomUserPrincipal principal = jwtTokenGeneratorAdapter.extractCustomUserPrincipal(jwtToken);
+
+                UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(
+                        principal,
+                        null,
+                        principal.getAuthorities()
+                );
+
+                authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+
+                SecurityContextHolder.getContext().setAuthentication(authToken);
+            }
+        } catch (Exception e) {
+            SecurityContextHolder.clearContext();
+        }
+
+        filterChain.doFilter(request, response);
+    }
+}
