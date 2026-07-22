4b3d2fa10fbb5283c4b56735243d98556f3d1039
isalvama <inesalvans@gmail.com>
2026-07-22 18:18:46 +0200

    fix: rename JwtAuthFilter class to JwtAuthenticationFilter

--- src/main/java/com/isalvama/fresh_keep/modules/account/infrastructure/security/token/JwtAuthenticationFilter.java
+++ src/main/java/com/isalvama/fresh_keep/modules/account/infrastructure/security/token/JwtAuthenticationFilter.java
@@ -1,6 +1,5 @@
 package com.isalvama.fresh_keep.modules.account.infrastructure.security.token;
 
-import com.isalvama.fresh_keep.modules.account.domain.model.Account;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -8,9 +7,7 @@ import jakarta.servlet.http.HttpServletResponse;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
@@ -19,7 +16,7 @@ import java.io.IOException;
 
 @Component
 @RequiredArgsConstructor
-public class JwtAuthFilter extends OncePerRequestFilter {
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
     private final JwtTokenGeneratorAdapter jwtTokenGeneratorAdapter;
 
