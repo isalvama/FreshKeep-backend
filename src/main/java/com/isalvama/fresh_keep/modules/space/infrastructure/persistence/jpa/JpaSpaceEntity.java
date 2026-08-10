8ab6be03e917d112fb2810f3b1299f11b26eb1d1
isalvama <inesalvans@gmail.com>
2026-08-10 17:55:05 +0200

    feat: create V2__fresh_keep_schema.sql file with creation of users, receipt_images, storage_spots, spaces, shopping_receipts, products, spaces_participants and product_storage_spot_history tables; create JpaSpaceEntity and JpaStorageSpotEntity

---
+++ src/main/java/com/isalvama/fresh_keep/modules/space/infrastructure/persistence/jpa/JpaSpaceEntity.java
@@ -0,0 +1,57 @@
+package com.isalvama.fresh_keep.modules.space.infrastructure.persistence.jpa;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.hibernate.type.SqlTypes;
+
+import java.time.Instant;
+import java.util.Set;
+import java.util.UUID;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "spaces")
+public class JpaSpaceEntity {
+
+    @Id
+    @Column(name = "id", updatable = false, nullable = false)
+    @JdbcTypeCode(SqlTypes.UUID)
+    private UUID id;
+
+    @Column(name = "name", nullable = false, length = 30)
+    private String name;
+
+    @Column(name = "creator_id", nullable = false, length = 30)
+    @JdbcTypeCode(SqlTypes.UUID)
+    private UUID creatorId;
+
+    @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
+    @JoinColumn(name = "space_id")
+    private Set<JpaStorageSpotEntity> storageSpots;
+
+    @ElementCollection
+    @CollectionTable(
+            name = "spaces_participants",
+            joinColumns = @JoinColumn(name = "space_id")
+    )
+    @Column(name = "participant_id")
+    @JdbcTypeCode(SqlTypes.UUID)
+    private Set<UUID> participantIds;
+
+    @CreationTimestamp
+    @Column(name = "created_at", updatable = false, nullable = false)
+    private Instant createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "last_updated_at")
+    private Instant lastUpdatedAt;
+}
---
+++ src/main/java/com/isalvama/fresh_keep/modules/space/infrastructure/persistence/jpa/JpaStorageSpotEntity.java
@@ -0,0 +1,32 @@
+package com.isalvama.fresh_keep.modules.space.infrastructure.persistence.jpa;
+
+import com.isalvama.fresh_keep.modules.space.domain.model.StorageSpotType;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.type.SqlTypes;
+
+import java.util.UUID;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "storage_spots")
+public class JpaStorageSpotEntity {
+    @Id
+    @JdbcTypeCode(SqlTypes.UUID)
+    @Column(name = "id", updatable = false, nullable = false)
+    private UUID id;
+
+    @Column(name = "name", nullable = false, length = 30)
+    private String name;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "storage_spot_type", nullable = false, length = 30)
+    private StorageSpotType type;
+}
