78a78e5f9788f25bd12cd44e41c8ebe9e9e007a9
isalvama <inesalvans@gmail.com>
2026-09-04 11:10:10 +0200

    fix: remove spaceId attribute from ProcessNewShoppingReceiptRequest, add cloudinary variables to application.properties

--- src/main/java/com/isalvama/fresh_keep/modules/shopping_receipt/infrastructure/web/dto/response/ProductResponse.java
+++ src/main/java/com/isalvama/fresh_keep/modules/shopping_receipt/infrastructure/web/dto/response/ProductResponse.java
@@ -1,5 +1,4 @@
 package com.isalvama.fresh_keep.modules.shopping_receipt.infrastructure.web.dto.response;
-import com.isalvama.fresh_keep.modules.shopping_receipt.application.port.out.dto.ProductExtraction;
 
 import java.time.LocalDate;
 
